41dcf5035bf13ce273df9b22f79a49c01a05a17a
Lorie Ann Pascual <lorie.ann.b.pascual@gmail.com>
2022-01-26 20:23:38 +0800

    added responsiveness to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,18 +6,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.home}>
       <Layout>
         <section className={styles.header}>
           <div>
             <h2>Design</h2>
             <h3>Develop & Deploy</h3>
             <p>Web Developer based in Rizal</p>
-            <Link className={styles.btn} to="/Projects">My Portfolio Projects</Link>&nbsp;&nbsp;&nbsp;
-            <Link className={styles.btn} to="https://drive.google.com/file/d/1zPTXjuJx0rpShEDF_JQopIBsCyPPLZ8v/view?usp=sharing" download target="_blank" type="button">Download my CV</Link>
+            <div className={styles.homeBtn}>
+            <Link className={styles.btn} to="/Projects">My Portfolio Projects</Link>
+            <Link className={styles.btn} to="https://drive.google.com/file/d/1zPTXjuJx0rpShEDF_JQopIBsCyPPLZ8v/view?usp=sharing" 
+                  download 
+                  target="_blank" 
+                  type="button"
+                  >Download my CV
+              </Link>
+            </div>
           </div>
           <figure>
-            <img src="/webdev.png" alt="banner" style={{maxWidth: '100%'}}/>
+            <img src="/webdev.png" alt="banner" style={{maxWidth: '100%'}} className={styles.banner}/>
           </figure>
         </section>
       </Layout>
